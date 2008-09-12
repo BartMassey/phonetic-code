9cec5f0eeed7b39a1d898e1384ba70e72ec0a49b
Bart Massey <bart@cs.pdx.edu>
2008-09-11 20:42:39 -0700

    finished first prototype of thimk; cleaned up repo

--- thimk.hs
+++ thimk.hs
@@ -1,14 +1,34 @@
+--- Spelling word suggestion tool
+--- Copyright © 2008 Bart Massey
+--- ALL RIGHTS RESERVED
+
+--- This software is licensed under the "3-clause ('new')
+--- BSD License".  Please see the file COPYING provided with
+--- this distribution for license terms.
+
 --- Idea and name from an old program that used to hang
 --- around Reed College, probably written by Graham Ross and
 --- now apparently lost in the mists of time.
 --- See
 ---   http://groups.google.com/group/net.sources/msg/8856593862fe22bd
---- for the one very vague reference I've found on the web.
+--- for the one very vague reference I've found on the web (in the
+--- SEE ALSO section of the referenced manpage).
 
 --- Soundex is designed for names, but seems to work about the same
 --- with other words.  I allow it to do arbitrary-length
 --- codes instead of length 4 by default, to get better accuracy.
 
+import System.Console.ParseArgs
 import Data.Soundex
 
-main = interact (unwords . map soundexNARA . words)
+data ArgIndex = ArgWord deriving (Eq, Ord, Show)
+
+argd = [ Arg { argIndex =ArgWord,
+               argName = Nothing,
+               argAbbr = Nothing,
+               argData = argDataRequired "word" ArgtypeString,
+               argDesc = "Word to be looked up." } ]
+
+main = do
+  args <- parseArgsIO ArgsComplete argd
+  print $ soundex True (getRequiredArg args ArgWord)
