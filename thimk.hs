4ec40ca8134609e2d595d92fcccc710148581c57
Bart Massey <bart@cs.pdx.edu>
2008-09-20 22:36:17 -0700

    got phonix with edit distance filtering working

--- thimk.hs
+++ thimk.hs
@@ -19,10 +19,18 @@
 --- codes instead of length 4 by default, to get better accuracy.
 
 import System.IO
+import Data.List
+import Data.Ord
+
+import Text.EditDistance
 import System.Console.ParseArgs
+
 import Text.PhoneticCode.Soundex
+import Text.PhoneticCode.Phonix
 
 data ArgIndex = ArgWord
+              | ArgCode
+              | ArgNoPrefilter
               | ArgDict
                 deriving (Eq, Ord, Show)
 
@@ -32,24 +40,54 @@ argd = [ Arg { argIndex = ArgDict,
                argData = argDataDefaulted "path" ArgtypeString
                            "/usr/share/dict/words",
                argDesc = "Dictionary file to search" },
+         Arg { argIndex = ArgCode,
+               argName = Just "code",
+               argAbbr = Just 'c',
+               argData = argDataDefaulted "phonetic-code" ArgtypeString
+                           "phonix",
+               argDesc = "Phonetic code: soundex, phonix"},
+         Arg { argIndex = ArgNoPrefilter,
+               argName = Just "no-distance-prefilter",
+               argAbbr = Nothing,
+               argData = Nothing,
+               argDesc = "Do not throw away wildly misspelled words before looking further."},
          Arg { argIndex = ArgWord,
                argName = Nothing,
                argAbbr = Nothing,
                argData = argDataRequired "word" ArgtypeString,
                argDesc = "Word to be looked up." } ]
 
-try_word word = unlines
+edit_distance s t =
+    restrictedDamerauLevenshteinDistance ec s t where
+        ec = EditCosts {
+               insertionCost = 2,
+               deletionCost = 2,
+               transpositionCost = 1,
+               substitutionCost = 3 }
+
+try_word :: Bool -> (String -> String) -> String -> String -> String
+try_word prefilter pcode word = unlines
+              . take 5
+              . sortBy (comparing ed)
               . map snd
-              . filter ((== sf word) . fst)
+              . filter ((== pcode word) . fst)
               . map sfs
+              . prefilter_f
               . lines
     where
-      sf = soundex True
-      sfs w = (sf w, w)
+      sfs w = (pcode w, w)
+      ed = edit_distance word
+      prefilter_f = if prefilter then filter ((<= 10) . ed) else id
 
 main = do
   args <- parseArgsIO ArgsComplete argd
   h <- argFileOpener (getRequiredArg args ArgDict) ReadMode 
   dict <- hGetContents h
+  let pcode = case getRequiredArg args ArgCode of
+                "soundex" -> soundex True
+                "phonix" -> phonix
+                c -> usageError args $ "unknown phonetic code " ++ c
+  let prefilter = not (gotArg args ArgNoPrefilter)
   let word = getRequiredArg args ArgWord
-  putStr $ try_word word dict
+  let result = try_word prefilter pcode word dict
+  putStr result
